1295c7f19261512f84f1e0cd2cf70343d1f2dbfd
yerasylbekenov <77799107+yerasylbekenov@users.noreply.github.com>
2025-02-11 21:35:40 +0300

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,12 +1,11 @@
-SENDER_EMAIL = "sayahat.app@yandex.kz"  # Ваш email
-SENDER_PASSWORD = "vtbunffvperhepwm"   # Ваш пароль или пароль приложения
-SMTP_SERVER = "smtp.yandex.ru"
+SENDER_EMAIL = ""  # Ваш email
+SENDER_PASSWORD = ""   # Ваш пароль или пароль приложения
+SMTP_SERVER = ""
 SMTP_PORT = 465
 DB_NAME = "database.db"
-IMAGEBB_API_KEY = "your_imagebb_api_key"
-AUTH_KEY = "YTk3MzI5YzItNzA4Ny00Mzg3LTljMzUtYWE5ZDJmNTA5ZDE2OjlmNGU2YzUxLWU5ZTUtNDZiMi1iZjExLTlmYzdiYWQwMzY5ZA=="
-IMG_KEY = "099aa2997d6647438ae54131d71afc08"
-PAYPAL_CLIENT_ID = "ASjRb0raYczEDeJfqh36bIg6ICkaykoz9WCnt5wZhqeTtQNJG8nA34k39mAQnmYY_YAR7Rf6uSxEgztc"
-PAYPAL_CLIENT_SECRET = "EMabAT1iEqs6hzqu4hfjZZGhqATFM4DgmluZX-Ul1SHrVKYVFPN22sFHGYRo9-pkRNu7pp5ZhLrA2wh1"
-PAYPAL_API_URL = "https://sandbox.paypal.com"
-SECRET_KEY = "POPA"
+AUTH_KEY = ""
+IMG_KEY = ""
+PAYPAL_CLIENT_ID = ""
+PAYPAL_CLIENT_SECRET = ""
+PAYPAL_API_URL = ""
+SECRET_KEY = ""
